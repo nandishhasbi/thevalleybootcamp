eb5946e70e36d3fcf3d242b38390ff2128e8beba
vishakbharadwaj94 <vishak.svec@gmail.com>
2020-11-24 22:25:46 +0530

    updates

--- landing/a.py
+++
@@ -1,8 +0,0 @@
-#subplotting for later
-import matplotlib.pyplot as plt
-
-for i in range(5):
-  data_pt = next(iter(dataset))
-  image = plt.imshow(data_pt[0])
-  label = [x for (x,y) in label_mapping.items() if y==data_pt[1]]
-  print(label)
